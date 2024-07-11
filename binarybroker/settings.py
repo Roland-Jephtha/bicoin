7939a1786152fa1fe830b2f39cda3328f0b28279
unknown <jephtroland832@gmail.com>
2024-07-11 19:55:04 +0100

    first commit

--- binarybroker/settings.py
+++ binarybroker/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR/ 'staticfiles_build', 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
